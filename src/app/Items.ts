9f7519d894cf964a31d07e9c3eba93adadfa6d55
XiZhu0204 <x295zhu@uwaterloo.ca>
2024-12-26 18:31:52 -0500

    init commit

---
+++ src/app/Items.ts
@@ -0,0 +1,14 @@
+import { DisplayCarouselItem } from "./DisplayCarousel";
+
+export const Items: DisplayCarouselItem[] = [
+    {
+        isImage: false,
+        content: "So it beings, the 0th day of a new love...",
+        associatedDay: 0,
+    },
+    {
+        isImage: true,
+        content: "/image0.jpg",
+        associatedDay: 0,
+    },
+]
